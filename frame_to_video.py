cc5e0c516fc938b3309c421055273249b07a6386
KaiJaz0326 <ongkaijaz2000@gmail.com>
2021-12-24 19:35:12 +0800

    convert mp4 video format

--- frame_to_video.py
+++ frame_to_video.py
@@ -2,14 +2,15 @@ import cv2
 import os
 
 image_folder = 'frames'
-video_name = 'output.avi'
+video_name = 'output.mp4'
 
 images = [img for img in sorted(os.listdir(image_folder), key=lambda x:x.split(".")[0][-3:])]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 fps = 25
 
-video = cv2.VideoWriter(video_name, 0, fps, (width,height))
+video = cv2.VideoWriter(video_name, fourcc, fps, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
